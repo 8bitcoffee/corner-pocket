06fcd83b568be5670350475acb64b57c9509a343
J Read <j@8bit.coffee>
2024-01-17 03:31:33 -0600

    next matchup displayed in team details

--- src/components/TeamPage/TeamPage.jsx
+++ src/components/TeamPage/TeamPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import './TeamPage.css';
 
 // Shows team roster with some base stats.
@@ -10,23 +11,40 @@ function TeamPage(){
     const teamId = useParams().teamid;
     const userTeam = useSelector(store=>store.userTeam);
 
+    const copyCode = () => {
+        let joinCode = userTeam.teamInfo.join_code;
+        let alertField = document.getElementById("copy-alert");
+        navigator.clipboard.writeText(joinCode);
+        alertField.innerHTML = "Code copied to clipboard";
+        setTimeout(()=>{alertField.innerHTML = ""}, 3000)
+    }
+
     useEffect(() => {
+        dispatch({type: "FETCH_TEAM", payload: {id: teamId}});
         dispatch({type: 'FETCH_LEAGUES'});
         dispatch({type: "FETCH_TEAMS"});
-        dispatch({type: "FETCH_TEAM", payload: {id: teamId}})
     }, []);
-
+    
     return(
-        <div>
-            <h3>{userTeam[0].team_name}</h3>
-            <h5>Join code - {userTeam[0].join_code}</h5> // TODO: Copy to clipboard with click. Add accompanying alert
-            <h5>Roster:</h5>
+        <div id="team-details-div">
+            <h3 id="team-name">{userTeam.teamInfo.team_name}</h3>
+            <h4>Join code - {userTeam.teamInfo.join_code} <ContentCopyIcon fontSize="small" id="copy-icon" onClick={copyCode}/></h4>
+            <p id="copy-alert"></p>
+            <h4>Roster:</h4>
             <ul>
-                {userTeam.map((user)=>{
+                {userTeam.roster.map((user)=>{
                     return(
                         <li key={user.id}>{user.first_name + " " + user.last_name}</li>
                 )})}
             </ul>
+            <br></br>
+            <h4>Next Matchup:</h4>
+            {userTeam.matchups.length == 0 ?
+                (<h5 className='details-subtitle' id="match-details">No Matchups scheduled</h5>):
+                (<div>
+                    <h5>{userTeam.nextOpponentInfo.date}</h5>
+                    <h5 className='details-subtitle' id="match-details">{`${userTeam.nextOpponentInfo.away_team_name} vs. ${userTeam.nextOpponentInfo.home_team_name}`}</h5>
+                </div>)}
         </div>
     )
 }
--- src/redux/sagas/getTeam.saga.js
+++ src/redux/sagas/getTeam.saga.js
@@ -1,10 +1,51 @@
 import axios from 'axios';
+import { DateTime } from 'luxon';
 import { put, takeLatest } from 'redux-saga/effects';
 
 function* getTeam(action){
     try {
-        const response = yield axios.get(`/api/team/${action.payload.id}`);
-        yield put({type: "SET_TEAM", payload: response.data});
+        let teamInfo = yield axios.get(`/api/team/${action.payload.id}`);
+        let roster = yield axios.get(`/api/team/roster/${action.payload.id}`);
+        let matchups = yield axios.get(`/api/match/team/${action.payload.id}`);
+
+        matchups = matchups.data;
+        teamInfo = teamInfo.data;
+        roster = roster.data;
+
+        let nextDate = DateTime.fromISO(matchups[0].date);
+        nextDate = nextDate.toISODate();
+
+        let opponentId = "";
+        let nextOpponentInfo;
+        let away_team_name;
+        let home_team_name;
+        if (matchups[0].away_team_id == action.payload.id){
+            away_team_name = teamInfo.team_name;
+            opponentId = matchups[0].home_team_id;
+        }
+        else {
+            opponentId = matchups[0].away_team_id;
+            home_team_name = teamInfo.team_name
+        }
+
+        if (opponentId == null){
+            nextOpponentInfo = {team_name: "BYE"}
+        }
+        else{
+            nextOpponentInfo = yield axios.get(`/api/team/${opponentId}`);
+            nextOpponentInfo = nextOpponentInfo.data;
+        }
+
+        if (away_team_name == teamInfo.team_name){
+            home_team_name = nextOpponentInfo.team_name;
+        }
+        else{
+            away_team_name = nextOpponentInfo.team_name;
+        }
+        nextOpponentInfo.date = nextDate;
+        nextOpponentInfo.home_team_name = home_team_name;
+        nextOpponentInfo.away_team_name = away_team_name;
+        yield put({type: "SET_TEAM", payload: {teamInfo: teamInfo, roster: roster, matchups: matchups, nextOpponentInfo, nextOpponentInfo}});
     }
     catch (error) {
         console.error("Error fetching team info", error);
