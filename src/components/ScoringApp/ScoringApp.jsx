fd14f6dafb6e2e779ed0dd89c143e79fc8230b24
J Read <j@8bit.coffee>
2024-03-14 15:55:57 -0500

    Added error message for lineup not set

--- src/components/ScoringApp/ScoringApp.jsx
+++ src/components/ScoringApp/ScoringApp.jsx
@@ -7,16 +7,18 @@ import './ScoringApp.css';
 // Base component for the scoring app
 // SetLineup, MatchView, and RoundView are all rendered from this component
 function ScoringApp(){
+    const userTeam = useSelector(store => store.userTeam);
     const dispatch = useDispatch();
     const history = useHistory();
     const matchupId = useParams().matchid;
-
     const user = useSelector(store=>store.user);
     const userId = user.id;
     const matchup = useSelector(store=>store.userMatchup);
+    const homeTeam = matchup.home.id;
+    const awayTeam = matchup.away.id;
     const homeCaptain = matchup.home.owner_id;
     const awayCaptain = matchup.away.owner_id;
-
+    const teamId = userTeam.teamInfo.id;
 
     useEffect(() => {
         dispatch({type: "FETCH_MATCHUP", payload: {id: matchupId}});
@@ -33,6 +35,17 @@ function ScoringApp(){
             <SetLineup team={matchup.away} roster={matchup.awayRoster} info={matchup.info}/>
         )
     }
+    else if(teamId == homeTeam && userId != homeCaptain && matchup.info[0].home_lineup_set == false){
+        return (
+            <h2>Lineup not yet set. Talk to your captain!</h2>
+        )
+    }
+    else if(teamId == awayTeam && userId != awayCaptain && matchup.info[0].away_lineup_set == false){
+        return (
+            <h4 id="lineup-not-set">Lineup not yet set. Talk to your captain!</h4>
+        )
+    }
+    
 }
 
 export default ScoringApp;
